8616915312712b2bfd79185c8abe2642259b3041
Geraldo Andrade <geraldo@geraldoandrade.com>
2019-12-26 16:20:51 +0100

    Created pre-receive server hook

--- start.sh
+++ start.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# If there is some public key in keys folder
-# then it copies its contain in authorized_keys file
 if [ "$(ls -A /git-server/keys/)" ]; then
   cd /home/git
   cat /git-server/keys/*.pub > .ssh/authorized_keys
@@ -10,8 +8,6 @@ if [ "$(ls -A /git-server/keys/)" ]; then
   chmod -R 600 .ssh/*
 fi
 
-# Checking permissions and fixing SGID bit in repos folder
-# More info: https://github.com/jkarlosb/git-server-docker/issues/1
 if [ "$(ls -A /git-server/repos/)" ]; then
   cd /git-server/repos
   chown -R git:git .
